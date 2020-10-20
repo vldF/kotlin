70dc99627b4dccce419371215d0eb5d6fbc363c6
vldf <vladislavf7@gmail.com>
2020-10-20 20:22:47 +0300

    [FIR] Introduce interprocedural analysis

---
+++ compiler/fir/checkers/src/org/jetbrains/kotlin/fir/analysis/cfa/InterproceduralVisitor.kt
@@ -0,0 +1,123 @@
+/*
+ * Copyright 2010-2020 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
+ */
+
+package org.jetbrains.kotlin.fir.analysis.cfa
+
+import org.jetbrains.kotlin.fir.declarations.FirControlFlowGraphOwner
+import org.jetbrains.kotlin.fir.declarations.FirProperty
+import org.jetbrains.kotlin.fir.declarations.FirPropertyAccessor
+import org.jetbrains.kotlin.fir.expressions.toResolvedCallableSymbol
+import org.jetbrains.kotlin.fir.references.FirResolvedNamedReference
+import org.jetbrains.kotlin.fir.resolve.dfa.cfg.*
+import org.jetbrains.kotlin.fir.resolve.dfa.controlFlowGraph
+import org.jetbrains.kotlin.fir.symbols.FirBasedSymbol
+import org.jetbrains.kotlin.fir.symbols.impl.FirNamedFunctionSymbol
+import org.jetbrains.kotlin.fir.symbols.impl.FirPropertySymbol
+
+abstract class InterproceduralVisitor<I : PathAwareControlFlowInfo<I, V>, V : ControlFlowInfo<V, *, *>> :
+    ControlFlowGraphVisitor<I, MutableMap<CFGNode<*>, I>>() {
+    internal fun onFunctionCall(
+        direction: TraverseDirection,
+        initialInfo: I,
+        nodeMap: MutableMap<CFGNode<*>, I>,
+        changed: MutableMap<CFGNode<*>, Boolean>,
+        functionsWhitelist: Collection<FirNamedFunctionSymbol>,
+        visitedSymbols: Collection<FirBasedSymbol<*>>,
+        callNode: CFGNode<*>,
+        visitedNodesOld: MutableSet<CFGNode<*>>
+    ) {
+        if (callNode is FunctionCallNode) {
+            val functionSymbol = callNode.fir.toResolvedCallableSymbol() as? FirNamedFunctionSymbol
+            val symbol = functionSymbol as? FirBasedSymbol<*> ?: return
+            val function = functionSymbol.fir as? FirControlFlowGraphOwner
+            val functionCfg = function?.controlFlowGraphReference?.controlFlowGraph
+            if (functionSymbol in functionsWhitelist && symbol !in visitedSymbols && function != null && functionCfg != null) {
+                val functionEnterNode = functionCfg.nodes.firstOrNull() ?: return
+                val functionExitNode = functionCfg.nodes.lastOrNull() ?: return
+                val nodeAfterCall = callNode.followingNodes.firstOrNull() ?: return
+                CFGNode.addEdgeIfNotExist(callNode, functionEnterNode, EdgeKind.Interprocedural, false)
+                CFGNode.addEdgeIfNotExist(functionExitNode, nodeAfterCall, EdgeKind.Interprocedural, false)
+
+                functionCfg.collectInterDataForNode(
+                    direction,
+                    initialInfo,
+                    this,
+                    nodeMap,
+                    changed,
+                    functionsWhitelist,
+                    visitedSymbols + symbol,
+                    visitedNodesOld
+                )
+            }
+        } else if (callNode is QualifiedAccessNode) {
+            val property = (callNode.fir.calleeReference as? FirResolvedNamedReference)?.resolvedSymbol?.fir as? FirProperty ?: return
+            val propertySymbol = property.symbol
+            val setter = property.setter ?: return
+            val getter = property.getter ?: return
+            if (propertySymbol !in visitedSymbols) {
+                if (setter.symbol !in visitedSymbols) {
+                    processAccessor(
+                        propertySymbol,
+                        direction,
+                        initialInfo,
+                        setter,
+                        nodeMap,
+                        changed,
+                        functionsWhitelist,
+                        visitedSymbols + setter.symbol + propertySymbol,
+                        callNode,
+                        visitedNodesOld
+                    )
+                }
+                if (getter.symbol !in visitedSymbols) {
+                    processAccessor(
+                        propertySymbol,
+                        direction,
+                        initialInfo,
+                        getter,
+                        nodeMap,
+                        changed,
+                        functionsWhitelist,
+                        visitedSymbols + getter.symbol + propertySymbol,
+                        callNode,
+                        visitedNodesOld
+                    )
+                }
+            }
+        }
+    }
+
+    private fun processAccessor(
+        propertySymbol: FirPropertySymbol,
+        direction: TraverseDirection,
+        initialInfo: I,
+        propertyAccessor: FirPropertyAccessor,
+        nodeMap: MutableMap<CFGNode<*>, I>,
+        changed: MutableMap<CFGNode<*>, Boolean>,
+        functionsWhitelist: Collection<FirNamedFunctionSymbol>,
+        visitedSymbols: Collection<FirBasedSymbol<*>>,
+        node: CFGNode<*>,
+        visitedNodesOld: MutableSet<CFGNode<*>>
+    ) {
+        val accessorCfg = propertyAccessor.symbol.fir.controlFlowGraphReference?.controlFlowGraph ?: return
+        val functionEnterNode = accessorCfg.nodes.firstOrNull() ?: return
+        val functionExitNode = accessorCfg.nodes.lastOrNull() ?: return
+        val nodeAfterCall = functionEnterNode.followingNodes.firstOrNull() ?: return
+
+        CFGNode.addEdgeIfNotExist(node, functionEnterNode, EdgeKind.Interprocedural, false)
+        CFGNode.addEdgeIfNotExist(functionExitNode, nodeAfterCall, EdgeKind.Interprocedural, false)
+        accessorCfg.collectInterDataForNode(
+            direction,
+            initialInfo,
+            this,
+            nodeMap,
+            changed,
+            functionsWhitelist,
+            visitedSymbols + propertySymbol,
+            visitedNodesOld
+        )
+    }
+}
+
